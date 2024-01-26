04d9c06644b542969ada7b5932170bf4a4a7f398
Creedyfish <ielbanbuenaofficial@gmail.com>
2024-01-26 21:44:22 +0800

    chore: changed "page" to "Page" of page.tsx  build error

--- multi-step-form-nextjs/app/page.tsx
+++ multi-step-form-nextjs/app/page.tsx
@@ -10,7 +10,7 @@ import Step1 from "./components/Step1";
 import Step2 from "./components/Step2";
 import Step3 from "./components/Step3";
 
-export default function page() {
+export default function Page() {
   const steps = [1, 2, 3, 4];
   const [formData, setFormData] = useState({});
   const [currentStep, setCurrentStep] = useState(3);
@@ -74,87 +74,3 @@ export default function page() {
     </main>
   );
 }
-
-// import Step1 from "./components/Step1";
-// import Step2 from "./components/Step2";
-// import Step3 from "./components/Step3";
-
-// export default function Home() {
-//   const [currentStep, setCurrentStep] = useState(1);
-//   const [formData, setFormData] = useState({});
-
-//   const steps = { 1: Step1, 2: Step2, 3: Step3, 4: Step1 };
-
-//   useEffect(() => {
-//     console.log(formData);
-//   }, [formData, currentStep]);
-
-//   return (
-//     <main className="flex text-base h-full w-full  xs:justify-center xs:items-center ">
-//       <div className="xs:p-10 p-0 xs:flex">
-//         <div className="absolute flex w-full justify-center items-center gap-5 p-6 text-white font-medium">
-//           {Object.keys(steps).map((step) => (
-//             <div
-//               key={step}
-//               className={`rounded-full text-sm border-[1px] w-8 h-8 flex items-center justify-center text-center ${
-//                 Number(step) === currentStep
-//                   ? "bg-light-blue text-marine-blue border-light-blue"
-//                   : ""
-//               }`}
-//               onClick={() => setCurrentStep(Number(step))}
-//             >
-//               {step}
-//             </div>
-//           ))}
-//         </div>
-//         <Image
-//           className="xs:hidden"
-//           width={375}
-//           height={172}
-//           src={"images/bg-sidebar-mobile.svg"}
-//           alt="Mobile-SideBar"
-//         />
-//         <Image
-//           className="xs:block hidden"
-//           width={375}
-//           height={172}
-//           src={"images/bg-sidebar-desktop.svg"}
-//           alt="Desktop-SideBar"
-//         />
-//         <div className="flex flex-col w-full h-full gap-3 bg-white p-4">
-//           {currentStep === 1 && (
-//     <section>
-//     <Step1
-//       setFormData={setFormData}
-//       formData={formData}
-//       setCurrentStep={setCurrentStep}
-//     />
-//   </section>
-// )}
-// {currentStep === 2 && (
-//   <section>
-//     <Step2
-//       setFormData={setFormData}
-//       formData={formData}
-//       setCurrentStep={setCurrentStep}
-//     />
-//   </section>
-// )}
-// {currentStep === 3 && (
-//   <section>
-//     ThIRD STEP
-//     <input type="text" className="bg-slate-400" />
-//     <button
-//       type="button"
-//       className="bg-red-500"
-//       onClick={() => setCurrentStep((prev: number) => prev - 1)}
-//     >
-//       Back
-//     </button>
-//   </section>
-// )}
-//         </div>
-//       </div>
-//     </main>
-//   );
-// }
